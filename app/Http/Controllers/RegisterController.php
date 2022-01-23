563baf252e4c602c2a28f116320d527d192a0fd6
sitaram kudireddy <sitaram.kudireddy@xcubelabs.com>
2022-01-23 09:24:33 +0530

    registration completed

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function register(Request $request)
+    {
+        // return $request->validated();
+         $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+        if (!$validator->fails()){
+            $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return $user;
+        }else{
+            return response($validator->errors(), 404);
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Register $register)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Register $register)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Register $register)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Register  $register
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Register $register)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("register",[RegisterController::class,'register']);
+
+
+/*
+to create controller with model
+php artisan make:controller PhotoController --resource --model=Photo
+
+to migrate the table
+php artisan make:migration create_profiles_table --create=profiles
+php artisan migrate
+*/
